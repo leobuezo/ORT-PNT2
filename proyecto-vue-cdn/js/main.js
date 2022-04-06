ccb444624c483fe8de8428115c0664b49ea57c2e
Leo Buezo <leobuezo@gmail.com>
2022-04-05 23:06:33 -0300

    Clase nro 4: Data Binding en VueJS

--- proyecto-vue-cdn/js/main.js
+++ proyecto-vue-cdn/js/main.js
@@ -8,7 +8,16 @@ new Vue({
         valor: 123,
         contador: 123,
         contador2: 456,
-        contador3: 789
+        contador3: 789,
+        mostrar: true,
+        mostrar2: true,
+        usuarios: ['Juan', 'Pedro', 'María', 'Esther', 'Haman'],
+        alumnos: [
+            {nombre: 'Juan', apellido: 'Perez', edad: 32, curso: false, foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/11_avatar-512.png'},
+            {nombre: 'Pedro', apellido: 'Raggio', edad: 32, curso: true, foto: 'https://www.iconfinder.com/icons/2754580/woman_business_woman_avatar_female_icon'},
+            {nombre: 'María', apellido: 'Ramos', edad: 32, curso: false, foto: 'https://www.iconfinder.com/icons/2754576/woman_female_avatar_icon'},
+            {nombre: 'Esther', apellido: 'Buessi', edad: 32, curso: true, foto: 'https://www.iconfinder.com/icons/2754579/business_man_man_avatar_icon'},
+        ],
     },
     methods: {
         saludar(event) { alert('Holaaa!!') },
@@ -16,6 +25,15 @@ new Vue({
         mostrarContador3() { return this.contador3}
     },
     computed: {
-
+        calcularAlunosCurso() {
+            let cant = 0
+            this.alumnos.forEach( alumno => {
+                if(alumno.curso) cant++
+            })
+            return {
+                cant: cant,
+                total: this.alumnos.length
+            }
+        }
     }
 });
